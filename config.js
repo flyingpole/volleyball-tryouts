947323f465eff0454eacda651c0e9a2a4f0c03e7
Darin Danelski <flyingpole@users.noreply.github.com>
2026-07-20 11:15:25 -0500

    Wire up the deployed Apps Script Web App URL
    Connects the frontend to the live Google Sheet backend.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste the Apps Script Web App URL here after deploying (see README.md).
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
 const CONFIG = {
-  SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwLiBs40BEjN8LCy1eUtIwgZy9glDuO9H2mMM2NuP2GVajKGvwjk-j6LLiSQQDBS5RI/exec",
 };
